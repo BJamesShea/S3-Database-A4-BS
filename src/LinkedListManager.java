189f27684bcf5532d59b229378b9af5c130c52a8
Brandon Shea <brandon.shea1994@icloud.com>
2025-02-03 20:19:47 -0330

    nested node class

--- src/LinkedListManager.java
+++ src/LinkedListManager.java
@@ -1,4 +1,19 @@
 public class LinkedListManager {
+
+    private class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+
+        }
+    }
+
+
     public static void main(String[] args) {
         System.out.println("Linked lists!");
     }
