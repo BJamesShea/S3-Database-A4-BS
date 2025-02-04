68d511ac896d2d53f7ff9a0916ab8db8adcf24ad
Brandon Shea <brandon.shea1994@icloud.com>
2025-02-03 20:56:27 -0330

    deleteNode function

--- src/LinkedListManager.java
+++ src/LinkedListManager.java
@@ -44,6 +44,50 @@ public class LinkedListManager {
     }
 
 
+    public void deleteNode(int data){
+        Node current = head;
+
+        while (current != null && current.data != data) {
+            current = current.next;
+        }
+
+        if (current == null) {
+            System.out.println("Node with value " + data + " not found");
+            return;
+        }
+
+        if (current == head) {
+            head = current.next;
+            if (head != null) {
+                head.prev = null;
+            } else {
+                tail = null;
+            }
+        }
+
+        else if (current == tail) {
+            tail = current.prev;
+            if (tail != null) {
+                tail.next = null;
+            } else
+            {
+                head = null;
+            }
+        }
+
+        else {
+            current.prev.next = current.next;
+            current.next.prev = current.prev;
+        }
+
+        System.out.println("Node with value " + data + " deleted.");
+
+
+
+
+    }
+
+
     public static void main(String[] args) {
         LinkedListManager list = new LinkedListManager();
 
@@ -52,6 +96,25 @@ public class LinkedListManager {
         list.addNode(30);
         list.addNode(40);
 
+        System.out.println();
+        System.out.println("OG list");
+        list.displayList();
+        System.out.println();
+
+        list.deleteNode(10);
+        list.displayList();
+        System.out.println();
+
+        list.deleteNode(40);
+        list.displayList();
+        System.out.println();
+
+        list.deleteNode(20);
+        list.displayList();
+        System.out.println();
+
+        System.out.println();
+        list.deleteNode(69420);
         list.displayList();
     }
 }
